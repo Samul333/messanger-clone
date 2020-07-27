2737856c8eaefbe7a331d6e15f3cb90fea6a8aff
Samul <samulshrestha101@gmail.com>
2020-07-28 00:55:41 +0545

    Updates

---
+++ precache-manifest.cf42c34d8d8ea41bfc5048345b5f8524.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b201c1eeef62e4740b5ba82acfabc419",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d774064d177cbe93b73f",
+    "url": "/static/css/main.f1928d75.chunk.css"
+  },
+  {
+    "revision": "a09731ec28cd1e3d2f3c",
+    "url": "/static/js/2.29254007.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.29254007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d774064d177cbe93b73f",
+    "url": "/static/js/main.b2a5c774.chunk.js"
+  },
+  {
+    "revision": "4269f17ef9618d6a4598",
+    "url": "/static/js/runtime-main.793539b7.js"
+  }
+]);
